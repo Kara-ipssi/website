2fd54d298f4c8aa4471f2e3c410cc46e2b659ee4
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-03-11 03:07:54 +0100

    add google analitycs

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-PX3QJM5');
-            </script>
-        <!-- End Google Tag Manager -->
+        {{-- Ajouter la balise google analitycs avec l'id : G-Z30TFVW40F --}}
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z30TFVW40F"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Z30TFVW40F');
+        </script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -47,10 +50,7 @@
         @livewireStyles
     </head>
     <body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="80">
-        <!-- Google Tag Manager (noscript) -->
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PX3QJM5"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+
         
         <!-- start navbar -->
         @livewire('navbar')
